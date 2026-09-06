006a94d4c458401bd3eb000348b5e3e63c659e2a
chesterlouie <chesterlouie.agulo@gmail.com>
2026-09-06 19:37:03 +0800

    Add store delivery receiving

--- scripts/migrate.mjs
+++ scripts/migrate.mjs
@@ -55,6 +55,7 @@ const migrations = [
   ["044_substitution_approvals", new URL("../database/migrations/044_substitution_approvals.sql", import.meta.url)],
   ["045_order_management_stores", new URL("../database/migrations/045_order_management_stores.sql", import.meta.url)],
   ["046_store_user_portal", new URL("../database/migrations/046_store_user_portal.sql", import.meta.url)],
+  ["047_store_delivery_receiving", new URL("../database/migrations/047_store_delivery_receiving.sql", import.meta.url)],
 ];
 const client = new pg.Client({ connectionString, database: databaseName });
 console.log(`Connecting to configured database ${client.connectionParameters.database}.`);
