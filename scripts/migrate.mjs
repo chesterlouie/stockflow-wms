79722e1334da8eb9e06222345b92d1fbe4b67e26
chesterlouie <chesterlouie.agulo@gmail.com>
2026-09-09 15:48:00 +0800

    Add carrier integration gateway

--- scripts/migrate.mjs
+++ scripts/migrate.mjs
@@ -87,6 +87,7 @@ const migrations = [
   ["076_order_release_planning", new URL("../database/migrations/076_order_release_planning.sql", import.meta.url)],
   ["077_carrier_delivery_tracking", new URL("../database/migrations/077_carrier_delivery_tracking.sql", import.meta.url)],
   ["078_carrier_assignment_tendering", new URL("../database/migrations/078_carrier_assignment_tendering.sql", import.meta.url)],
+  ["079_carrier_integration_gateway", new URL("../database/migrations/079_carrier_integration_gateway.sql", import.meta.url)],
 ];
 const client = new pg.Client({ connectionString, database: databaseName });
 console.log(`Connecting to configured database ${client.connectionParameters.database}.`);
